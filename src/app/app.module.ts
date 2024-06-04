475cec1c9cb002752132245e75066a95169070fd
ecossio <ecossio@outlook.com>
2024-06-03 17:59:17 -0700

    Utilizar Hash (#) en el ruteo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { provideHttpClient } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [AppRoutingModule, BrowserModule, SharedModule],
-  providers: [provideHttpClient()],
+  providers: [provideHttpClient(),{provide: LocationStrategy, useClass:HashLocationStrategy}],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
